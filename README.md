fa9065a8216fab388ddf2b202304ed2879fc0d47
Coin-Gulter <duvalder230@gmail.com>
2023-04-10 18:03:42 +0300

    Added possibility to save paramets and change setting

---
+++ README.md
@@ -0,0 +1,33 @@
+    
+    MONI-TUNE.....
+
+    Its a program that use linear regression algoritm of machine learning to autotune your screen bright
+in term of changing your screen from dark to light theame and vise-verse.
+
+    After download moni-tune startup with user log in.
+
+    To control there are three hotkeys:
+
+        PAUSE - "ctrl+alt+c"
+        UNPAUSE - "ctrl+alt+v"
+        CLOSE - "ctrl+alt+x"
+        CHANGE SETTING = "ctrl+alt+z"
+
+    Moni-tune remember your last 30 actions of changing your screen bright and the screens at that moment and 
+depends on this information learn what screen bright you need for this screen.
+
+    SETIING:
+
+Also you can change some setting. 
+    You can enter number of saved screen which means the more number the more accuracy
+program predict brithness value but worse adapting to the change. 
+    And you can enter diviation value. It depending to the value stay more or less 
+sensitive to the screen changing.
+
+    FILES:
+
+    After first start app create two files. The first one is "app_setting.json" and its a setting of program.
+The second is "bright_recog_reg_model.pkl" and its a saved state of machine learning module that uses for prediction.
+
+
+        ENJOY AND SEE YOU LATER :)
--- main.py
+++ main.py
@@ -3,40 +3,62 @@ import numpy as np
 import screen_brightness_control as sbc
 import keyboard
 import time
+import os
+import pickle
+import json
 from sklearn import linear_model
-from windows_toasts import InteractableWindowsToaster, ToastText1
+from windows_toasts import InteractableWindowsToaster, ToastText1, ToastInputTextBox, ToastButton
 
 
 get_avg_screen_color = lambda: np.array(pyautogui.screenshot()).mean()
 
 class monitor_bright_control():
     def __init__(self):
-        self.trained = False
         self.program_work = True
         self.program_paused = False
 
-        self.max_trainig_objects = 15
-        self.training_avg_color_db = np.array([[55.]])
-        self.training_screen_bright_db = np.array([40.])
+        self.training_avg_color_db = np.array([[100.]])
+        self.training_screen_bright_db = np.array([50.])
 
         self.last_bright = sbc.get_brightness()
         self.last_avg_color = get_avg_screen_color()
-        self.img_reg_model = linear_model.LinearRegression()
 
-        self.avg_color_diviation = 15
+        if os.path.isfile('app_setting.json'):
+            with open('app_setting.json', 'r', encoding='utf-8') as outfile:
+                self.setting = json.load(outfile)
+        else:
+            self.setting = {"max_screen_object":30, "avg_color_diviation":15}
+            with open("app_setting.json", "w", encoding="utf-8") as infile:
+                json.dump(self.setting, infile)
+
+        if os.path.isfile('bright_recog_reg_model.pkl'):
+            self.img_reg_model = pickle.load(open("bright_recog_reg_model.pkl", "rb"))
+            self.trained = True
+        else:
+            self.img_reg_model = linear_model.LinearRegression()
+            self.trained = False
 
         self.interacteble_toaster = InteractableWindowsToaster('Moni-tune')
         self.toast_notification = ToastText1()
+        self.toast_input_box = ToastText1()
+        self.toast_input_box.SetBody(f"Your setting now:\nSaved screens: {self.setting['max_screen_object']}\nDiviation: {self.setting['avg_color_diviation']}")
+        self.toast_input_box.AddInput(ToastInputTextBox('max_screen_object', 'Saved screen from 5 to 200:', 'only number'))
+        self.toast_input_box.AddInput(ToastInputTextBox('avg_color_diviation', 'Diviation value from 5 to 200:', 'only number'))
+        self.toast_input_box.AddAction(ToastButton('submit', 'submit'))
+        self.toast_input_box.on_activated = self.accept_new_setting
         self.show_notification = False
+        self.notification_setting = False
         self.notification_text = ''
 
         keyboard.add_hotkey('ctrl+alt+c', self.pause_program)
         keyboard.add_hotkey('ctrl+alt+v', self.alive_program)
         keyboard.add_hotkey('ctrl+alt+x', self.close_program)
+        keyboard.add_hotkey('ctrl+alt+z', self.additional_setting)
+        
 
     def insert_in_db(self, current_avg_screen_color, current_bright):
 
-        if len(self.training_screen_bright_db) == self.max_trainig_objects:
+        if len(self.training_screen_bright_db) == self.setting["max_screen_object"]:
             self.training_avg_color_db = np.delete(self.training_avg_color_db, 0, 0)
             self.training_screen_bright_db = np.delete(self.training_screen_bright_db, 0)
 
@@ -48,7 +70,8 @@ class monitor_bright_control():
 
     def start_program(self):
 
-        self.set_notification('Moni-tune working')
+        self.notification_text = 'Moni-tune working'
+        self.set_notification()
 
         while self.program_work:
             time.sleep(0.5)
@@ -69,7 +92,9 @@ class monitor_bright_control():
                     self.time_now = time.time()
                     self.trained = True
 
-                elif abs(current_avg_screen_color-self.last_avg_color) > self.avg_color_diviation:
+                    pickle.dump(self.img_reg_model, open("bright_recog_reg_model.pkl", "wb"))
+
+                elif abs(current_avg_screen_color-self.last_avg_color) > self.setting["avg_color_diviation"]:
 
                     print('predict')
                     predict_avg_screen_array = np.array([current_avg_screen_color]).reshape((1,1))
@@ -84,38 +109,63 @@ class monitor_bright_control():
                     self.time_now = time.time()
 
             if self.show_notification:
-                self.toast_notification.SetBody(self.notification_text)
-                self.interacteble_toaster.show_toast(self.toast_notification)
+                if self.notification_setting:
+                    self.toast_input_box.SetBody(f"Your setting now:\nSaved screens: {self.setting['max_screen_object']}\nDiviation: {self.setting['avg_color_diviation']}")
+                    self.interacteble_toaster.show_toast(self.toast_input_box)
+                    self.notification_setting = False
+                else:
+                    self.toast_notification.SetBody(self.notification_text)
+                    self.interacteble_toaster.show_toast(self.toast_notification)
 
                 self.show_notification = False
 
-    def set_notification(self, text):
+    def set_notification(self):
         self.show_notification = True
-        self.notification_text = text
+
+    def additional_setting(self):
+        self.notification_setting = True
+        self.show_notification = True
+
+    def accept_new_setting(self, activatedEventArgs):
+        temp_setting = activatedEventArgs.inputs
+        for key in temp_setting:
+            value = temp_setting[key]
+            if value.isdigit():
+                value_num = int(value)
+                if value_num<=200 and value_num>=5 :
+                    self.setting[key] = value_num
+
+        with open("app_setting.json", "w", encoding="utf-8") as infile:
+            json.dump(self.setting, infile)
 
     def close_program(self):
         self.program_work = False
-        self.set_notification('Moni-tune closed')
+        self.notification_text = 'Moni-tune closed'
+        self.set_notification()
 
     def pause_program(self):
         if self.program_paused:
             print('Program already paused')
-            self.set_notification('Moni-tune already paused')
+            self.notification_text = 'Moni-tune already paused'
+            self.set_notification()
 
         else:
             print('Program paused...')
             self.program_paused = True
-            self.set_notification('Moni-tune paused...')
+            self.notification_text = 'Moni-tune paused...'
+            self.set_notification()
     
     def alive_program(self):
         if not self.program_paused:
             print('Program already alive')
-            self.set_notification('Moni-tune already unpaused')
+            self.notification_text = 'Moni-tune already unpaused'
+            self.set_notification()
 
         else:
             print('Program now ALIIIIIIVE!!!')
             self.program_paused = False
-            self.set_notification('Moni-tune now ALIVE!!!')
+            self.notification_text = 'Moni-tune now ALIVE!!!'
+            self.set_notification()
 
 
 if __name__ == "__main__":
